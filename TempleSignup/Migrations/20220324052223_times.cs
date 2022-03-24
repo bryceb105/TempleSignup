219cbfa44c1ca767ef091d8d0a35076bea1df9f2
rcoles14 <90427464+rcoles14@users.noreply.github.com>
2022-03-23 23:47:42 -0600

    form

---
+++ TempleSignup/Migrations/20220324052223_times.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TempleSignup.Migrations
+{
+    public partial class times : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "responses",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    DateTime = table.Column<string>(nullable: true),
+                    GroupSize = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_responses", x => x.id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "id", "DateTime", "Email", "GroupSize", "Name", "Phone" },
+                values: new object[] { 1, "April 1st, 2022, 8:00am", "colesmith@email.com", 4, "Smiths", "775-222-111" });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "id", "DateTime", "Email", "GroupSize", "Name", "Phone" },
+                values: new object[] { 9, "April 1st, 2022, 3:00pm", "youth@email.com", 12, "Youth Group", null });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "id", "DateTime", "Email", "GroupSize", "Name", "Phone" },
+                values: new object[] { 3, "April 1st, 2022, 9:00am", null, 0, null, null });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "id", "DateTime", "Email", "GroupSize", "Name", "Phone" },
+                values: new object[] { 4, "April 1st, 2022, 10:00am", null, 0, null, null });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "id", "DateTime", "Email", "GroupSize", "Name", "Phone" },
+                values: new object[] { 5, "April 1st, 2022, 11:00am", null, 0, null, null });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "id", "DateTime", "Email", "GroupSize", "Name", "Phone" },
+                values: new object[] { 6, "April 1st, 2022, 12:00pm", null, 0, null, null });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "id", "DateTime", "Email", "GroupSize", "Name", "Phone" },
+                values: new object[] { 7, "April 1st, 2022, 1:00pm", null, 0, null, null });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "id", "DateTime", "Email", "GroupSize", "Name", "Phone" },
+                values: new object[] { 8, "April 1st, 2022, 2:00pm", null, 0, null, null });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "id", "DateTime", "Email", "GroupSize", "Name", "Phone" },
+                values: new object[] { 10, "April 1st, 2022, 4:00pm", null, 0, null, null });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "id", "DateTime", "Email", "GroupSize", "Name", "Phone" },
+                values: new object[] { 11, "April 1st, 2022, 5:00pm", null, 0, null, null });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "id", "DateTime", "Email", "GroupSize", "Name", "Phone" },
+                values: new object[] { 12, "April 1st, 2022, 6:00pm", null, 0, null, null });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "id", "DateTime", "Email", "GroupSize", "Name", "Phone" },
+                values: new object[] { 13, "April 1st, 2022, 7:00pm", null, 0, null, null });
+
+            migrationBuilder.InsertData(
+                table: "responses",
+                columns: new[] { "id", "DateTime", "Email", "GroupSize", "Name", "Phone" },
+                values: new object[] { 14, "April 1st, 2022, 8:00pm", null, 0, null, null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "responses");
+        }
+    }
+}
--- TempleSignup/Models/TempleContext.cs
+++ TempleSignup/Models/TempleContext.cs
@@ -14,97 +14,9 @@ namespace TempleSignup.Models
         }
 
         public DbSet<TempleModel> responses { get; set; }
-        public DbSet<Time> times { get; set; }
 
         protected override void OnModelCreating(ModelBuilder mb)
         {
-            mb.Entity<Time>().HasData(
-
-                new Time
-                {
-                    id = 1,
-                    DateTime = "April 1st, 2022, 8:00AM",
-                    Available = false
-                    
-                },
-                                new Time
-                                {
-                                    id = 2,
-                                    DateTime = "April 1st, 2022, 9:00AM",
-                                    Available = false
-
-                                },
-                                new Time
-                                {
-                                    id = 3,
-                                    DateTime = "April 1st, 2022, 10:00AM",
-                                    Available = true
-
-                                }, new Time
-                                {
-                                    id = 4,
-                                    DateTime = "April 1st, 2022, 11:00AM",
-                                    Available = true
-
-                                }, new Time
-                                {
-                                    id = 5,
-                                    DateTime = "April 1st, 2022, 12:00PM",
-                                    Available = true
-
-                                }, new Time
-                                {
-                                    id = 6,
-                                    DateTime = "April 1st, 2022, 1:00PM",
-                                    Available = true
-
-                                }, new Time
-                                {
-                                    id = 7,
-                                    DateTime = "April 1st, 2022, 2:00PM",
-                                    Available = true
-
-                                }, new Time
-                                {
-                                    id = 8,
-                                    DateTime = "April 1st, 2022, 3:00PM",
-                                    Available = true
-
-                                }, new Time
-                                {
-                                    id = 9,
-                                    DateTime = "April 1st, 2022, 4:00PM",
-                                    Available = true
-
-                                }, new Time
-                                {
-                                    id = 10,
-                                    DateTime = "April 1st, 2022, 5:00PM",
-                                    Available = true
-
-                                }, new Time
-                                {
-                                    id = 11,
-                                    DateTime = "April 1st, 2022, 6:00PM",
-                                    Available = true
-
-                                }, new Time
-                                {
-                                    id = 12,
-                                    DateTime = "April 1st, 2022, 7:00PM",
-                                    Available = true
-
-                                }, new Time
-                                {
-                                    id = 13,
-                                    DateTime = "April 1st, 2022, 8:00PM",
-                                    Available = true
-
-                                }
-
-
-                );
-
 
 
 
@@ -113,19 +25,77 @@ namespace TempleSignup.Models
                 {
                     id = 1,
                     Name = "Smiths",
-                    TimeId = 1,
+                    DateTime = "April 1st, 2022, 8:00am",
                     GroupSize = 4,
                     Email = "colesmith@email.com",
                     Phone = "775-222-111"
                 },
                 new TempleModel
                 {
-                    id = 2,
+                    id = 9,
                     Name = "Youth Group",
-                    TimeId = 2,
+                    DateTime = "April 1st, 2022, 3:00pm",
                     GroupSize = 12,
                     Email = "youth@email.com"
+                },
+                new TempleModel
+                {
+                    id = 3,
+                    DateTime = "April 1st, 2022, 9:00am",
+                },
+                new TempleModel
+                {
+                    id = 4,
+                    DateTime = "April 1st, 2022, 10:00am",
+                },
+                new TempleModel
+                {
+                    id = 5,
+                    DateTime = "April 1st, 2022, 11:00am",
+                },
+                new TempleModel
+                {
+                    id = 6,
+                    DateTime = "April 1st, 2022, 12:00pm",
+                },
+                new TempleModel
+                {
+                    id = 7,
+                    DateTime = "April 1st, 2022, 1:00pm",
+                },
+                new TempleModel
+                {
+                    id = 8,
+                    DateTime = "April 1st, 2022, 2:00pm",
+                },
+                new TempleModel
+                {
+                    id = 10,
+                    DateTime = "April 1st, 2022, 4:00pm",
+                },
+                new TempleModel
+                {
+                    id = 11,
+                    DateTime = "April 1st, 2022, 5:00pm",
+                },
+                new TempleModel
+                {
+                    id = 12,
+                    DateTime = "April 1st, 2022, 6:00pm",
+                },
+                new TempleModel
+                {
+                    id = 13,
+                    DateTime = "April 1st, 2022, 7:00pm",
+                },
+                new TempleModel
+                {
+                    id = 14,
+                    DateTime = "April 1st, 2022, 8:00pm",
                 }
+
+
+
 
                 );
         }
